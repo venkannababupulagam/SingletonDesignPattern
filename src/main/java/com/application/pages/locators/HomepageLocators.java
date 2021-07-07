2bd5fc1cd58443e83aef4beac6575b8062ef507a
Venkannababu Pulagam <Venkannababu.Pulagam@ivycomptech.com>
2021-07-07 07:29:36 +0100

    Revert "Selenium Java Basic Framework"
    This reverts commit e25db6ef1b766eb480d33ef439e275621016216a.

--- src/main/java/com/application/pages/locators/HomepageLocators.java
+++
@@ -1,7 +0,0 @@
-package com.application.pages.locators;
-
-import org.openqa.selenium.By;
-
-public interface HomepageLocators {
-By HOMEPAGE_LOGO = By.xpath("//div[@class='login_logo']");
-}
--- src/main/java/com/application/utils/commons/CommonVariables.java
+++
@@ -1,8 +0,0 @@
-package com.application.utils.commons;
-
-public class CommonVariables {
-    public static final String projectPath = System.getProperty("user.dir");
-    public static final String configfilePath = projectPath + "/configfiles/config.properties";
-    public static final String LOGIN_DATA = projectPath + "/src/main/resources/TestData/TestUsers.xlsx";
-    public static final String SHEET_NAME = "USER_DETAILS";
-}
